db6c39ad9f21e54465df2c5bad2768327f00c276
Aliaskar <urakov.aliaskar@gmail.com>
2020-09-13 19:00:03 +0600

    lesson 30 finish

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import {DOMListener} from '@/core/DOMListener';
+
+export class ExcelComponent extends DOMListener {
+
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import '@/styles/style.scss'
+import { Excel } from '@/components/excel/Excel';
+import { Header } from '@/components/header/Header';
+import { Toolbar } from '@/components/toolbar/Toolbar';
+import { Formula } from '@/components/formula/Formula';
+import { Table } from '@/components/table/Table';
+import '@/styles/style.scss';
 
-console.log('index.js');
+const excel = new Excel('#app', {
+  components: []
+})
 
+console.log('Excel', excel)
